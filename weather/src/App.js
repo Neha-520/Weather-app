8665ed2e39085da4793bd29e0dd07f99e5ec8a2f
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2021-07-23 01:37:05 +0530

    Temp component

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Temp from "./Temp"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+    <Temp/>
+    </>
   );
 }
 
 export default App;
+// api.openweathermap.org/data/2.5/weather?q=Sangrur&appid=ba81e597b2312de7e67e7937ee5f0438
+
+// 
---
+++ weather/src/Temp.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./style.css";
+
+export default function Temp () {
+    return (
+        <>
+        <div className="wrap">
+           <div className="search">
+               <input type="search" placeholder="Search..." autoFocus  className="searchTerm" id="search"></input>
+               <button className="searchButton" type="button">Search</button>        
+           </div> 
+        </div>
+        <div className="">
+
+        </div>
+        </>
+    )
+}
